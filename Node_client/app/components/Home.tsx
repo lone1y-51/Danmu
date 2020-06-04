10fa690c044f33406b3615187cc3dd84a1f918b8
lone1y <251639692@qq.com>
2020-06-04 19:59:04 +0800

    弹幕展示

--- Node_client/app/components/Home.tsx
+++ Node_client/app/components/Home.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 import { Buffer } from 'buffer';
 import SingleDM from './SingleDM';
 import {Card} from 'antd';
 
+const wss = new WebSocket("wss://danmuproxy.douyu.com:8504/");
 function Utf8ArrayToStr(array: Uint8Array) {
     var out, i, len, c;
     var char2, char3;
@@ -13,60 +14,59 @@ function Utf8ArrayToStr(array: Uint8Array) {
     len = array.length;
     i = 0;
     while(i < len) {
-    c = array[i++];
-    switch(c >> 4)
-    { 
-      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-        // 0xxxxxxx
-        out += String.fromCharCode(c);
-        break;
-      case 12: case 13:
-        // 110x xxxx   10xx xxxx
-        char2 = array[i++];
-        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-        break;
-      case 14:
-        // 1110 xxxx  10xx xxxx  10xx xxxx
-        char2 = array[i++];
-        char3 = array[i++];
-        out += String.fromCharCode(((c & 0x0F) << 12) |
-                       ((char2 & 0x3F) << 6) |
-                       ((char3 & 0x3F) << 0));
-        break;
+        c = array[i++];
+        switch(c >> 4)
+        { 
+            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+                // 0xxxxxxx
+                out += String.fromCharCode(c);
+                break;
+            case 12: case 13:
+                // 110x xxxx   10xx xxxx
+                char2 = array[i++];
+                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                break;
+            case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = array[i++];
+                char3 = array[i++];
+                out += String.fromCharCode(((c & 0x0F) << 12) |
+                            ((char2 & 0x3F) << 6) |
+                            ((char3 & 0x3F) << 0));
+                break;
+        }
     }
-    }
- 
     return out;
 }
 
 function genDanmuMessage(message: string) {
 	console.log("send message: ", message)
 	let dataLen = message.length + 9
-  let byte = new ArrayBuffer(dataLen + 4);
-  let data = new DataView(byte);
-  let messageBuf = new Buffer(message);
-  data.setUint32(0, dataLen, true);
-  data.setUint32(4, dataLen, true);
-  data.setUint8(8, 0xb1);
-  data.setUint8(9, 0x02);
-  data.setUint8(10, 0x00);
-  data.setUint8(11, 0x00);
-  messageBuf.forEach((ele: number, index: number) => {
-    data.setUint8(12+index, ele);
-  })
-  data.setUint8(dataLen + 4 -1, 0x00);
-	return data.buffer;
+    let byte = new ArrayBuffer(dataLen + 4);
+    let data = new DataView(byte);
+    let messageBuf = new Buffer(message);
+    data.setUint32(0, dataLen, true);
+    data.setUint32(4, dataLen, true);
+    data.setUint8(8, 0xb1);
+    data.setUint8(9, 0x02);
+    data.setUint8(10, 0x00);
+    data.setUint8(11, 0x00);
+    messageBuf.forEach((ele: number, index: number) => {
+        data.setUint8(12+index, ele);
+    })
+    data.setUint8(dataLen + 4 -1, 0x00);
+    return data.buffer;
 }
 
 function loginMessage(){
-  let message = "type@=loginreq/roomid@=623499/";
-  let buf = genDanmuMessage(message);
-  return buf;
+    let message = "type@=loginreq/roomid@=74960/";
+    let buf = genDanmuMessage(message);
+    return buf;
 }
 function joinGroupMessage(){
-  let message = "type@=joingroup/rid@=623499/gid@=-9999/";
-  let buf = genDanmuMessage(message);
-  return buf;
+    let message = "type@=joingroup/rid@=74960/gid@=-9999/";
+    let buf = genDanmuMessage(message);
+    return buf;
 }
 function messageHandle(message: string){
     let msgArray = message.split('/');
@@ -81,7 +81,6 @@ function messageHandle(message: string){
 
 export default function Home() {
     const [dm, setDm] = useState(Array<Map<any, any> >());
-    const wss = new WebSocket("wss://danmuproxy.douyu.com:8504/");
     wss.onmessage = (evt) => {
         evt.data.stream().getReader().read().then((val: any) => {
             let buf = val.value.slice(12, val.value.length-1);
@@ -89,26 +88,26 @@ export default function Home() {
             let dataMap = messageHandle(dataText);
             if(dataMap.get("type") == "loginres"){
                 wss.send(joinGroupMessage());
+                setInterval(function(){wss.send(genDanmuMessage("type@=mrkl/"))}, 45 *1000)
                 return;
             }else if(dataMap.get("type") == "chatmsg"){
-                // dm.push(dataMap.get('txt'));
-                console.log(dataMap.get('txt'));
+                console.log(dm.length);
                 setDm([
                     ...dm,
                     dataMap
-                ]);
+                ])
+                // dm.push(dataMap);
             }
         });
     }
-  useEffect(() => {
-    wss.onopen = () => {
-      console.log("connect to danmu server success");
-      wss.send(loginMessage());
-    }
-  });
-  return (
-    <div>
-        <Card title={null} className={styles.danmuContainer}>
+    useEffect(() => {
+        wss.onopen = () => {
+            console.log("connect to danmu server success");
+            wss.send(loginMessage());
+        }
+    });
+    return (
+        <div className={styles.danmuContainer} >
             <div>
                 {
                     dm.map((val: Map<any, any>) => {
@@ -116,8 +115,7 @@ export default function Home() {
                     })
                 }
             </div>
-        </Card>
-      {/* <Link to={routes.COUNTER}>to Counter</Link> */}
-    </div>
+        {/* <Link to={routes.COUNTER}>to Counter</Link> */}
+        </div>
   );
 }
--- Node_client/app/components/SingleDM.tsx
+++ Node_client/app/components/SingleDM.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 export default function SingleDM(props: {content: Map<any, any>}){
+    let backgroud = props.content.get('rg') ? 'gray' : undefined;
     return(
-        <div>{props.content.get('txt')}</div>
+        <p style={{fontSize: 18, backgroundColor: backgroud}} >
+            {props.content.get('rg') == '4' ? <span style={{color: 'yellow'}} >[房] </span> : null}
+            {props.content.get('bl') != '0' ? <span style={{color: 'yellow'}}>[{props.content.get('bl')} {props.content.get('bnn')}] </span> : null}
+            <span>Lv {props.content.get('level')} </span>
+            <span style={{color: 'green'}}>{props.content.get('nn')}: </span>
+            <span>{props.content.get('txt')} </span>
+        </p>
+        // <p>{props.content.get('txt')}</p>
     );
 }
